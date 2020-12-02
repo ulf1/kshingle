6e09b21d48bd96920ee107a039e7a67b90ba9cad
UH <554c46@gmail.com>
2020-12-02 15:26:41 +0100

    bumb version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 
 setup(name='kshingle',
-      version='0.1.0',
+      version='0.2.0',
       description='lSplit strings into (character-based) k-shingles',
       long_description=read('README.md'),
       long_description_content_type='text/markdown',
