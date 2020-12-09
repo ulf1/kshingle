56b50d0e09c406d36265e2934bcaae7a3240110b
UH <554c46@gmail.com>
2020-12-09 20:36:49 +0100

    obsolete func removed

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
 from setuptools import setup
 import m2r
-import os
-
-
-def get_readme():
-    return m2r.parse_from_file('README.md')
 
 
 setup(name='kshingle',
-      version='0.5.0',
+      version='0.5.1',
       description="Split strings into (character-based) k-shingles",
-      long_description=get_readme(),
+      long_description=m2r.parse_from_file('README.md'),
       long_description_content_type='text/x-rst',
       url='http://github.com/ulf1/kshingle',
       author='Ulf Hamster',
