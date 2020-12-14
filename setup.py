be6ed326642437f87740d684326abc9e4fd00451
UH <554c46@gmail.com>
2020-12-14 18:11:33 +0100

    fix #15

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup
-import m2r
+import os
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
 setup(name='kshingle',
-      version='0.6.0',
+      version='0.6.1',
       description="Split strings into (character-based) k-shingles",
-      long_description=m2r.parse_from_file('README.md'),
-      long_description_content_type='text/x-rst',
+      long_description=read('README.md'),
+      long_description_content_type='text/markdown',
       url='http://github.com/ulf1/kshingle',
       author='Ulf Hamster',
       author_email='554c46@gmail.com',
@@ -14,7 +18,6 @@ setup(name='kshingle',
       packages=['kshingle'],
       install_requires=[
           'setuptools>=40.0.0',
-          'm2r>=0.2.1',
           'numba>=0.52.0'
       ],
       python_requires='>=3.6',
