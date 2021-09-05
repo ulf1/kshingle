fee3c5634c76c3461057e82b9a7b88afd616c7fe
UH <554c46@gmail.com>
2021-09-05 15:24:07 +0200

    bump version

--- kshingle/__init__.py
+++ kshingle/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.8.2'
+__version__ = '0.8.3'
 
 from .shingleseqs import (shingleseqs_k, shingleseqs_list, shingleseqs_range)
 from .shingleset import (shingleset_k, shingleset_range, shingleset_list)
